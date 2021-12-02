5d3a9cc58230c599dc1119f033a704508219391b
sanath-kumar <sanathkumar790@gmail.com>
2021-12-02 15:52:35 +0530

    API update

--- lib/helpers/config.dart
+++ lib/helpers/config.dart
@@ -1,3 +1,4 @@
 class Config{
-  static String BASE_URL = "http://3.140.221.246:5011/api/v1";
+//  static String BASE_URL = "http://3.140.221.246:5011/api/v1";
+    static String BASE_URL = "http://api.staging.chickywok.co.in/api/v1";
 }
